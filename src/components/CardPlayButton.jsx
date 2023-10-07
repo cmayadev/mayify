c6e0fbce1cff02f687d18ef6c17bed372151aabb
Chris Maya <cmayadev@gmail.com>
2023-10-07 16:58:40 +0200

    current song / play playlist

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,47 @@
+import { Pause, Play } from "./Player"
+import { usePlayerStore } from "@/store/playerStore"
+
+export function CardPlayButton ({id}) {
+    const { 
+        currentMusic, 
+        isPlaying, 
+        setIsPlaying, 
+        setCurrentMusic
+    } = usePlayerStore(state => state)
+
+    const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
+
+    const handleClick = () => {
+        if (isPlayingPlaylist) {
+            setIsPlaying(false)
+            return
+        }
+
+        fetch(`/api/get-info-playlist.json?id=${id}`)
+            .then(res => res.json())
+            .then(data => {
+                const { songs, playlist } = data
+                setIsPlaying(true)
+                setCurrentMusic({
+                    playlist,
+                    songs,
+                    song: songs[0]
+                })
+            })
+
+        setCurrentMusic({
+            playlist: {
+                id
+            }
+        })
+
+    }
+
+    
+
+    return (
+        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
+            {isPlayingPlaylist ? <Pause /> : <Play />}
+        </button>
+    )
+}
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,38 +1,58 @@
 
 import PauseIcon from '@/icons/Pause.astro'
+import { usePlayerStore } from '@/store/playerStore'
 import { useEffect, useRef, useState } from 'react'
 
-const Play = () => (
+export const Play = () => (
     <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" class="Svg-sc-ytk21e-0 haNxPq"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
 )
 
-const Pause = () => (
+export const Pause = () => (
     <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" class="Svg-sc-ytk21e-0 haNxPq"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
 )
 
+const CurrentSong = ({ image, title, artists }) => {
+    return (
+        <div className={`flex items-center relative overflow-hidden`}>
+            <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden mr-2">
+                <img src={image} alt={title} />
+            </picture>
+
+            <div className='grid items-center mx-2'>
+                <h3 className='font-base text-sm block'>{title}</h3>
+                <span className='text-xs opacity-80'>{artists?.join(', ')}</span>
+            </div>
+        </div>
+    )
+}
+
 export function Player () {
-    const [isPlaying, setIsPlaying] = useState(false)
-    const [currentSong, setCurrentSong] = useState(null)
+    const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(state => state)
     const audioRef = useRef()
 
     useEffect(() => {
-        audioRef.current.src = `/music/1/01.mp3`
-    }, [])
+        isPlaying 
+            ? audioRef.current.play() 
+            : audioRef.current.pause()
+    }, [isPlaying])
+
+    useEffect(() => {
+        const {song, playlist, songs } = currentMusic
+        if (song) {
+            const src = `/music/${playlist?.id}/0${song.id}.mp3`
+            audioRef.current.src = src
+            audioRef.current.play()
+        }
+    }, [currentMusic])
 
     const handleClick = () => {
-        if (isPlaying) {
-            audioRef.current.pause()
-        } else {
-            audioRef.current.play()
-            audioRef.current.volume = 0.1
-        }
         setIsPlaying(!isPlaying)
     }
 
     return (
         <div className="flex flex-row justify-between w-full px-4 z-50">
             <div>
-                CurrentSong ...
+                <CurrentSong {...currentMusic.song} />
             </div>
 
             <div className="grid place-content-center gap-4 flex-1">
