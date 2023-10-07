595cf4229684baa7f423b9a668a846a4615a1ea8
Chris Maya <cmayadev@gmail.com>
2023-10-07 17:37:37 +0200

    control volume

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,73 +1,169 @@
-
-import PauseIcon from '@/icons/Pause.astro'
-import { usePlayerStore } from '@/store/playerStore'
-import { useEffect, useRef, useState } from 'react'
+import PauseIcon from "@/icons/Pause.astro";
+import { usePlayerStore } from "@/store/playerStore";
+import { useEffect, useRef, useState } from "react";
+import { Slider } from "./Slider";
 
 export const Play = () => (
-    <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" class="Svg-sc-ytk21e-0 haNxPq"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
-)
+  <svg
+    role="img"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    viewBox="0 0 16 16"
+    data-encore-id="icon"
+    class="Svg-sc-ytk21e-0 haNxPq"
+  >
+    <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
+  </svg>
+);
 
 export const Pause = () => (
-    <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" class="Svg-sc-ytk21e-0 haNxPq"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
-)
+  <svg
+    role="img"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    viewBox="0 0 16 16"
+    data-encore-id="icon"
+    class="Svg-sc-ytk21e-0 haNxPq"
+  >
+    <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
+  </svg>
+);
+
+export const Volume = () => (
+  <svg
+    fill="currentColor"
+    role="presentation"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    aria-label="Volumen medio"
+    id="volume-icon"
+    viewBox="0 0 16 16"
+    data-encore-id="icon"
+    class="Svg-sc-ytk21e-0 haNxPq"
+  >
+    <path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 6.087a4.502 4.502 0 0 0 0-8.474v1.65a2.999 2.999 0 0 1 0 5.175v1.649z"></path>
+  </svg>
+);
+
+export const Muted = () => (
+  <svg
+    fill="currentColor"
+    role="presentation"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    aria-label="Volumen apagado"
+    id="volume-icon"
+    viewBox="0 0 16 16"
+    data-encore-id="icon"
+    class="Svg-sc-ytk21e-0 haNxPq"
+  >
+    <path d="M13.86 5.47a.75.75 0 0 0-1.061 0l-1.47 1.47-1.47-1.47A.75.75 0 0 0 8.8 6.53L10.269 8l-1.47 1.47a.75.75 0 1 0 1.06 1.06l1.47-1.47 1.47 1.47a.75.75 0 0 0 1.06-1.06L12.39 8l1.47-1.47a.75.75 0 0 0 0-1.06z"></path>
+    <path d="M10.116 1.5A.75.75 0 0 0 8.991.85l-6.925 4a3.642 3.642 0 0 0-1.33 4.967 3.639 3.639 0 0 0 1.33 1.332l6.925 4a.75.75 0 0 0 1.125-.649v-1.906a4.73 4.73 0 0 1-1.5-.694v1.3L2.817 9.852a2.141 2.141 0 0 1-.781-2.92c.187-.324.456-.594.78-.782l5.8-3.35v1.3c.45-.313.956-.55 1.5-.694V1.5z"></path>
+  </svg>
+);
 
 const CurrentSong = ({ image, title, artists }) => {
-    return (
-        <div className={`flex items-center relative overflow-hidden`}>
-            <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden mr-2">
-                <img src={image} alt={title} />
-            </picture>
+  return (
+    <div className={`flex items-center relative overflow-hidden`}>
+      <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden mr-2">
+        <img src={image} alt={title} />
+      </picture>
 
-            <div className='grid items-center mx-2'>
-                <h3 className='font-base text-sm block'>{title}</h3>
-                <span className='text-xs opacity-80'>{artists?.join(', ')}</span>
-            </div>
-        </div>
-    )
-}
+      <div className="grid items-center mx-2">
+        <h3 className="font-base text-sm block">{title}</h3>
+        <span className="text-xs opacity-80">{artists?.join(", ")}</span>
+      </div>
+    </div>
+  );
+};
 
-export function Player () {
-    const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(state => state)
-    const audioRef = useRef()
+const VolumeControl = () => {
+  const volume = usePlayerStore((state) => state.volume);
+  const setVolume = usePlayerStore((state) => state.setVolume);
+  const previousVolumeRef = useRef(volume);
 
-    useEffect(() => {
-        isPlaying 
-            ? audioRef.current.play() 
-            : audioRef.current.pause()
-    }, [isPlaying])
+  const isVolumeMuted = volume === 0;
 
-    useEffect(() => {
-        const {song, playlist, songs } = currentMusic
-        if (song) {
-            const src = `/music/${playlist?.id}/0${song.id}.mp3`
-            audioRef.current.src = src
-            audioRef.current.play()
-        }
-    }, [currentMusic])
-
-    const handleClick = () => {
-        setIsPlaying(!isPlaying)
+  const handleVolume = () => {
+    if (isVolumeMuted) {
+      setVolume(previousVolumeRef.current);
+    } else {
+      previousVolumeRef.current = volume;
+      setVolume(0);
     }
+  };
 
-    return (
-        <div className="flex flex-row justify-between w-full px-4 z-50">
-            <div>
-                <CurrentSong {...currentMusic.song} />
-            </div>
+  return (
+    <div className="flex justify-center gap-x-2 text-white">
+      <button onClick={handleVolume}>
+        {volume === 0 ? <Muted /> : <Volume />}
+      </button>
+      <Slider
+        defaultValue={[50]}
+        max={100}
+        min={0}
+        className="w-[95px]"
+        onValueChange={(value) => {
+          const [newVolume] = value;
+          const volumeValue = newVolume / 100;
+          setVolume(volumeValue);
+        }}
+      />
+    </div>
+  );
+};
 
-            <div className="grid place-content-center gap-4 flex-1">
-                <div className='flex justify-center'>
-                    <button className='bg-white rounded-full p-2' onClick={handleClick}>
-                        {isPlaying ? <Pause /> : <Play />}
-                    </button>
-                </div>
-            </div>
+export function Player() {
+  const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(
+    (state) => state
+  );
+  const audioRef = useRef();
 
-            <div className="grid place-content-center">
-                Volumen
-            </div>
+  useEffect(() => {
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  }, [isPlaying]);
 
-            <audio ref={audioRef} />
+  useEffect(() => {
+    audioRef.current.volume = volume;
+  }, [volume]);
+
+  useEffect(() => {
+    const { song, playlist, songs } = currentMusic;
+    if (song) {
+      const src = `/music/${playlist?.id}/0${song.id}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.volume = volume;
+      audioRef.current.play();
+    }
+  }, [currentMusic]);
+
+  const handleClick = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  return (
+    <div className="flex flex-row justify-between w-full px-4 z-50">
+      <div>
+        <CurrentSong {...currentMusic.song} />
+      </div>
+
+      <div className="grid place-content-center gap-4 flex-1">
+        <div className="flex justify-center">
+          <button className="bg-white rounded-full p-2" onClick={handleClick}>
+            {isPlaying ? <Pause /> : <Play />}
+          </button>
         </div>
-    )
+      </div>
+
+      <div className="grid place-content-center">
+        <VolumeControl />
+      </div>
+
+      <audio ref={audioRef} />
+    </div>
+  );
 }
--- src/store/playerStore.js
+++ src/store/playerStore.js
@@ -1,8 +1,10 @@
-import { create } from "zustand"
+import { create } from "zustand";
 
 export const usePlayerStore = create((set) => ({
-    isPlaying: false,
-    currentMusic: { playlist: null, song: null, songs: [] },
-    setIsPlaying:  (isPlaying) => set({ isPlaying }),
-    setCurrentMusic: (currentMusic) => set({ currentMusic })
-}))
+  isPlaying: false,
+  currentMusic: { playlist: null, song: null, songs: [] },
+  setIsPlaying: (isPlaying) => set({ isPlaying }),
+  setCurrentMusic: (currentMusic) => set({ currentMusic }),
+  volume: 1,
+  setVolume: (volume) => set({ volume }),
+}));
